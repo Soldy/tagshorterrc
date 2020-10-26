19751000131ef6de9601d751b2361a1f1fba550a
Soldy <4786022+Soldy@users.noreply.github.com>
2020-10-26 01:49:52 +0000

    basic
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ index.js
@@ -0,0 +1,32 @@
+
+const tagshorterBase = function () {
+     let objects = {
+         'functions':[],
+         'classes'  :[],
+         'ids'      :[],
+         'names'    :[],
+         'langs'    :[]
+     }
+     let tags = {
+         'functions':'F',
+         'classes'  :'C',
+         'ids'      :'I',
+         'names'    :'N',
+         'langs'    :'L'
+     }
+     let serials = {
+         'functions':'F',
+         'classes'  :'C',
+         'ids'      :'I',
+         'names'    :'N',
+         'langs'    :'L'
+     }
+     let sample = 
+         'functions':'DF_',
+         'classes'  :'DC_',
+         'ids'      :'DI_',
+         'names'    :'DN_',
+         'langs'    :'DL_'
+     }
+
+}
